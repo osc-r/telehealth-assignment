96e302d2033bc8f08ab775246454181e5090be1c
oscar <osx@Oscars-MacBook-Pro-2.local>
2025-03-05 04:23:04 +0700

    add: welcome screen component

--- screens/Welcome/Welcome.screen.tsx
+++ screens/Welcome/Welcome.screen.tsx
@@ -1,8 +1,17 @@
 import { View } from "react-native";
 import { styles } from "./welcome.styles";
+import { Card, Text } from "react-native-paper";
 
 type WelcomeScreenProps = {};
 
 export const WelcomeScreen = (props: WelcomeScreenProps) => {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <Card style={styles.card}>
+        <Text variant="headlineLarge" style={styles.text}>
+          ยินดีตอนรับ
+        </Text>
+      </Card>
+    </View>
+  );
 };
--- screens/Welcome/welcome.styles.ts
+++ screens/Welcome/welcome.styles.ts
@@ -1,5 +1,14 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: "white" },
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  card: { padding: 16, backgroundColor: "white", shadowOpacity: 0.25 },
+  text: {
+    color: "#3C6CE7",
+  },
 });
