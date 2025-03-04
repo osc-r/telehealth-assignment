1ee0ca6a1bc6d6af5a1022494d5cdd4f0a388e97
oscar <osx@Oscars-MacBook-Pro-2.local>
2025-03-05 04:13:57 +0700

    add: login screen component

--- screens/Login/Login.screen.tsx
+++ screens/Login/Login.screen.tsx
@@ -1,8 +1,30 @@
-import { View } from "react-native";
+import { useState } from "react";
+import { Image, View } from "react-native";
 import { styles } from "./login.styles";
+import { Button, Text, TextInput } from "react-native-paper";
 
 type LoginScreenProps = {};
 
 export const LoginScreen = (props: LoginScreenProps) => {
-  return <View style={styles.container}></View>;
+  const [text, setText] = useState("");
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("@/assets/images/telehealth-logo.png")}
+        style={styles.logo}
+      />
+      <Text style={styles.headerText}>เข้าสู่ระบบเพื่อใช้งาน</Text>
+      <TextInput
+        label="เลขบัตรประจำตัวประชาชน/เลขหนังสือเดินทาง"
+        value={text}
+        onChangeText={(text) => setText(text)}
+      />
+      <Button>เข้าสู่ระบบ</Button>
+      <View style={styles.register}>
+        <Text>ยังไม่มีบัญชี ? </Text>
+        <Button>ลงทะเบียน</Button>
+      </View>
+    </View>
+  );
 };
--- screens/Login/login.styles.ts
+++ screens/Login/login.styles.ts
@@ -2,4 +2,7 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: { flex: 1, backgroundColor: "white" },
+  logo: { width: 80, height: 104 },
+  headerText: { fontSize: 18, fontWeight: 600 },
+  register: { flexDirection: "row", alignItems: "center" },
 });
